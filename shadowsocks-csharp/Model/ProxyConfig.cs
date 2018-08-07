0eb6b3d0270bad07ec6df0efc6af08e627c281ba
nicenight <nicenight@nicenight.cc>
2018-08-07 20:41:00 +0800

    init

---
+++ shadowsocks-csharp/Model/ProxyConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shadowsocks.Model
+{
+    [Serializable]
+    public class ProxyConfig
+    {
+        public const int PROXY_SOCKS5 = 0;
+        public const int PROXY_HTTP = 1;
+
+        public bool useProxy;
+        public int proxyType;
+        public string proxyServer;
+        public int proxyPort;
+
+        public ProxyConfig()
+        {
+            useProxy = false;
+            proxyType = PROXY_SOCKS5;
+            proxyServer = "";
+            proxyPort = 0;
+        }
+    }
+}
